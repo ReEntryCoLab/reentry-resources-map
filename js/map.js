63c3d14e8e5e3e664a986c90d9b433e5b018c888
Melissa Mcewen <melissa@equivoice.com>
2017-10-18 19:02:47 -0500

    generalizing the download guide jquery into nav.js and adding it to the general template

--- js/map.js
+++ js/map.js
@@ -142,13 +142,7 @@ $(function() {
     window.print();
   });
 
-  $("#download-guide").on("click", function() {
-    var source = $('#modal-guide-template').html();
-    var template = Handlebars.compile(source);
-    var result = template({});
-    $('#modal-pop').html(result);
-    $('#modal-pop').modal();
-  });
+
 });
 
 function makeSelectData(array) {
